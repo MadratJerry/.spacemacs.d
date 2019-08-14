70fbb9d100da13d912dff3eba229b55bc3c71fc0
Yotau Hong <crazymousethief@outlook.com>
2019-08-15 00:08:48 +0800

    ✨ Add some layers

--- init.el
+++ init.el
@@ -39,19 +39,20 @@ This function should only modify configuration layer settings."
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
-     ;; better-defaults
+     auto-completion
+     better-defaults
      emacs-lisp
+     osx
      ;; git
      helm
      ;; markdown
      multiple-cursors
      ;; org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
+     (shell :variables
+            shell-default-height 30
+            shell-default-position 'bottom)
      ;; spell-checking
-     ;; syntax-checking
+     syntax-checking
      treemacs
      ;; version-control
      )
